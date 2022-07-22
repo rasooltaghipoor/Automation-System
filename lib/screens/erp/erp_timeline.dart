dcc76344689ccd74afdd5f9704a2aba71ea1f942
rasooltaghipoor <rasool.taghipoor@gmail.com>
2022-07-22 16:06:24 +0430

    Clean code and fix a vertical line issue

--- lib/screens/erp/erp_timeline.dart
+++ lib/screens/erp/erp_timeline.dart
@@ -47,29 +47,6 @@ class ErpTimeline extends StatelessWidget {
     }
   }
 
-  // Icon getIcon(int index) {
-  //   if (index == _processIndex) {
-  //     return Icon(
-  //       Icons.timer_outlined,
-  //       size: 60,
-  //       color: getColor(index),
-  //     );
-  //   } else if (index < _processIndex) {
-  //     return Icon(
-  //       Icons.check_circle_outline,
-  //       size: 60,
-  //       color: getColor(index),
-  //     );
-  //     ;
-  //   } else {
-  //     return Icon(
-  //       Icons.do_not_touch_outlined,
-  //       size: 60,
-  //       color: getColor(index),
-  //     );
-  //   }
-  // }
-
   Widget getCircularIndicator(int index) {
     if (index == _processIndex) {
       return Stack(alignment: AlignmentDirectional.center, children: [
@@ -185,7 +162,9 @@ class ErpTimeline extends StatelessWidget {
               Container(
                 width: 5,
                 height: _lineLength,
-                color: index >= items.length - 1
+                color: index >= items.length - 1 ||
+                        (index == _processIndex &&
+                            _progressState == ProgressState.Rejected)
                     ? Colors.white
                     : index < _processIndex
                         ? Colors.green
@@ -304,13 +283,6 @@ class ErpTimeline extends StatelessWidget {
                     color: getColor(index),
                   ),
                 ),
-                // Text(
-                //   items[index].command,
-                //   style: TextStyle(
-                //     fontWeight: FontWeight.bold,
-                //     color: getColor(index),
-                //   ),
-                // ),
               ],
             )),
       ],
@@ -323,73 +295,8 @@ class ErpTimeline extends StatelessWidget {
       _isDesktop!
           ? rowList.add(horizontalProgressItem(i))
           : rowList.add(verticalProgressItem(i));
-      // rowList.add(Container(
-      //   width: 50,
-      //   height: 5,
-      //   color: Colors.red,
-      // ));
     }
     return rowList;
-  }
-
-  Widget generateIndicator(int index) {
-    var color;
-    var child;
-    if (index == _processIndex) {
-      color = inProgressColor;
-      child = const Padding(
-        padding: EdgeInsets.all(8.0),
-        child: CircularProgressIndicator(
-          strokeWidth: 3.0,
-          valueColor: AlwaysStoppedAnimation(Colors.white),
-        ),
-      );
-    } else if (index < _processIndex) {
-      color = completeColor;
-      child = const Icon(
-        Icons.check,
-        color: Colors.white,
-        size: 15.0,
-      );
-    } else {
-      color = todoColor;
-    }
-
-    if (index <= _processIndex) {
-      return Stack(
-        children: [
-          CustomPaint(
-            size: const Size(30.0, 30.0),
-            painter: _BezierPainter(
-              color: color,
-              drawStart: index > 0,
-              drawEnd: index < _processIndex,
-            ),
-          ),
-          DotIndicator(
-            size: 30.0,
-            color: color,
-            child: child,
-          ),
-        ],
-      );
-    } else {
-      return Stack(
-        children: [
-          CustomPaint(
-            size: const Size(15.0, 15.0),
-            painter: _BezierPainter(
-              color: color,
-              drawEnd: index < items.length - 1,
-            ),
-          ),
-          OutlinedDotIndicator(
-            borderWidth: 4.0,
-            color: color,
-          ),
-        ],
-      );
-    }
   }
 
   Widget build(BuildContext context) {
@@ -423,76 +330,5 @@ class ErpTimeline extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: generateTimelineItems(),
           );
-  }
-}
-
-/// hardcoded bezier painter
-/// TODO: Bezier curve into package component
-class _BezierPainter extends CustomPainter {
-  const _BezierPainter({
-    required this.color,
-    this.drawStart = true,
-    this.drawEnd = true,
-  });
-
-  final Color color;
-  final bool drawStart;
-  final bool drawEnd;
-
-  Offset _offset(double radius, double angle) {
-    return Offset(
-      radius * cos(angle) + radius,
-      radius * sin(angle) + radius,
-    );
-  }
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final paint = Paint()
-      ..style = PaintingStyle.fill
-      ..color = color;
-
-    final radius = size.width / 2;
-
-    var angle;
-    var offset1;
-    var offset2;
-
-    var path;
-
-    if (drawStart) {
-      angle = 3 * pi / 4;
-      offset1 = _offset(radius, angle);
-      offset2 = _offset(radius, -angle);
-      path = Path()
-        ..moveTo(offset1.dx, offset1.dy)
-        ..quadraticBezierTo(0.0, size.height / 2, -radius,
-            radius) // TODO connector start & gradient
-        ..quadraticBezierTo(0.0, size.height / 2, offset2.dx, offset2.dy)
-        ..close();
-
-      canvas.drawPath(path, paint);
-    }
-    if (drawEnd) {
-      angle = -pi / 4;
-      offset1 = _offset(radius, angle);
-      offset2 = _offset(radius, -angle);
-
-      path = Path()
-        ..moveTo(offset1.dx, offset1.dy)
-        ..quadraticBezierTo(size.width, size.height / 2, size.width + radius,
-            radius) // TODO connector end & gradient
-        ..quadraticBezierTo(size.width, size.height / 2, offset2.dx, offset2.dy)
-        ..close();
-
-      canvas.drawPath(path, paint);
-    }
-  }
-
-  @override
-  bool shouldRepaint(_BezierPainter oldDelegate) {
-    return oldDelegate.color != color ||
-        oldDelegate.drawStart != drawStart ||
-        oldDelegate.drawEnd != drawEnd;
   }
 }
